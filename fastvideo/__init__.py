e96589b2f48f46344c77b13c89a0667571ec5f57
Chao Ma <mc8305@gmail.com>
2026-05-05 03:44:35 +0000

    feat: restore Wan inference CLI (VideoGenerator + multiprocess worker)

--- fastvideo/__init__.py
+++ fastvideo/__init__.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from fastvideo.api.sampling_param import SamplingParam
 from fastvideo.configs.pipelines import PipelineConfig
+from fastvideo.entrypoints.video_generator import VideoGenerator
 from fastvideo.version import __version__
 
-__all__ = ["PipelineConfig", "SamplingParam", "__version__"]
+__all__ = ["PipelineConfig", "SamplingParam", "VideoGenerator", "__version__"]
---
+++ fastvideo/worker/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+from .executor import Executor
+from .multiproc_executor import MultiprocExecutor
+
+__all__ = ["Executor", "MultiprocExecutor"]
